2b1cde8186f6d1c3ef2f01da8730b05083b5263d
juan-lien-do <findmejuan@hotmail.com>
2024-07-08 19:40:40 -0300

    primera clase

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.hasNextInt()){
+            int numerito = sc.nextInt();
+            System.out.println(numerito+1);
+        }
+        else {
+            System.out.println("ta mal escrito");
+        }
+
+
+
+
+
     }
 }
